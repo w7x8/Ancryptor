edd70731f300927d79184e5d0290a0e506b6272a
Boody <69832947+w7x8@users.noreply.github.com>
2022-11-14 00:55:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,53 +3,25 @@ import keyboard
 import pyperclip
 
 mode = input("a.Enycrption b.Dycrption: ")
-
-def encrypt():
-    passwd = input('Insert your password here: ')
-
-    encrypted = passwd.replace('a', '!',).replace('b', '%').replace('c', '(').replace('d', '+').replace('e', '|').replace('f', '#').replace('g', '~').replace('h', '`').replace('i', '@').replace('j', '^').replace('k', ',').replace('l', '{').replace('m', '<').replace('n', '-').replace('o', '[').replace('p', '"').replace('q', ':').replace('r', ';').replace('s', '?').replace('t', '؟').replace('u', '$').replace('v', ',').replace('w', '.').replace('x', ']').replace('y', '}').replace('z', ')')
-
-    time.sleep(0.5)
-    print("Here is your encrypted password: " + encrypted)
-    time.sleep(0.5)
-    time.sleep(0.5)
-    print("Password coppied to clipboard!")
-
-    pyperclip.copy(encrypted)
-
-    time.sleep(0.5)
-    print("Press enter to exit")
-
-    while True:
-        if keyboard.is_pressed('enter'):
-            break
-
-
-def dycrypt():
-    passwd = input('Insert your password here: ')
+passwd = input('Insert your password here: ')
     
-    dycrypted = passwd.replace('!', 'a',).replace('%', 'b').replace('(', 'c').replace('+', 'd').replace('|', 'e').replace('#', 'f').replace('~', 'g').replace('`', 'h').replace('@', 'i').replace('^', 'j').replace(',', 'k').replace('{', 'l').replace('<', 'm').replace('-', 'n').replace('[', 'o').replace('"', 'p').replace(':', 'q').replace(';', 'r').replace('?', 's').replace('؟', 't').replace('$', 'u').replace(',', 'v').replace('.', 'w').replace(']', 'x').replace('}', 'y').replace(')', 'z')
-
-    time.sleep(0.5)
-    print("Here is your dycrypted password: " + dycrypted)
-    time.sleep(0.5)
-    time.sleep(0.5)
-    print("Password coppied to clipboard!")
-
-    pyperclip.copy(dycrypted)
-
-    time.sleep(0.5)
-    print("Press enter to exit")
-
-    while True:
-        if keyboard.is_pressed('enter'):
-            break
-
-
-if mode == "a":
-    encrypt()
-
-elif mode == "b":
-    dycrypt()
+if mode == 'a':
+    ancrypt = passwd.replace('a', '!',).replace('b', '%').replace('c', '(').replace('d', '+').replace('e', '|').replace('f', '#').replace('g', '~').replace('h', '`').replace('i', '@').replace('j', '^').replace('k', ',').replace('l', '{').replace('m', '<').replace('n', '-').replace('o', '[').replace('p', '"').replace('q', ':').replace('r', ';').replace('s', '?').replace('t', '؟').replace('u', '$').replace('v', ',').replace('w', '.').replace('x', ']').replace('y', '}').replace('z', ')')
+elif mode == 'b':
+    ancrypt = passwd.replace('!', 'a',).replace('%', 'b').replace('(', 'c').replace('+', 'd').replace('|', 'e').replace('#', 'f').replace('~', 'g').replace('`', 'h').replace('@', 'i').replace('^', 'j').replace(',', 'k').replace('{', 'l').replace('<', 'm').replace('-', 'n').replace('[', 'o').replace('"', 'p').replace(':', 'q').replace(';', 'r').replace('?', 's').replace('؟', 't').replace('$', 'u').replace(',', 'v').replace('.', 'w').replace(']', 'x').replace('}', 'y').replace(')', 'z')
 else:
     print(f"mode not recognized ({mode})")
+
+time.sleep(0.5)
+print("Here is your encrypted password: " + ancrypt)
+time.sleep(0.5)
+print("Password coppied to clipboard!")
+
+pyperclip.copy(ancrypt)
+
+time.sleep(0.5)
+print("Press enter to exit")
+
+while True:
+    if keyboard.is_pressed('enter'):
+        break
